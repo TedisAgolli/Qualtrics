fbe995555b615dedfb364cd75c781229bc7fb12a
TedisAgolli <tagolli@udel.edu>
2016-12-03 15:51:05 -0500

    added identical image

--- src/GenerateQuestion.java
+++ src/GenerateQuestion.java
@@ -24,6 +24,7 @@ public class GenerateQuestion {
         String header = "[[AdvancedFormat]]\r\n" +
                 "[[Block:Image Comparison]]\r\n";
 
+        String identicalHeader = "[[Block: Identical Images]]\r\n";
 
         // The name of the file to open.
         String fileName = "questions.txt";
@@ -38,8 +39,7 @@ public class GenerateQuestion {
             BufferedWriter bufferedWriter =
                     new BufferedWriter(fileWriter);
 
-            //Writing to file
-            bufferedWriter.append(header);
+
 
             Random r = new Random();
             int Low = 0;
@@ -52,6 +52,9 @@ public class GenerateQuestion {
 
             Document doc = Jsoup.connect(link).get();
 
+
+        /*   bufferedWriter.append(header);
+            //Img Comparison
             for(Element element :  doc.select("a[href~=(?i)\\.(dir)]") )
             {
                  doc = Jsoup.connect(link+"/" + element.attr("href")).get();
@@ -62,19 +65,34 @@ public class GenerateQuestion {
                     Result2 = 1-Result;
                     bufferedWriter.append(makeImgComparison(element.attr("href") + files.get(i + Result).attr("href"),element.attr("href") +files.get(i+Result2).attr("href")));
                 }
-            }
+            }*/
 
+
+            //Identical Comparison
+
+   /*         bufferedWriter.append(identicalHeader);
+
+            for(Element element :  doc.select("a[href~=(?i)\\.(dir)]") )
+            {
+                doc = Jsoup.connect(link+"/" + element.attr("href")).get();
+                files = doc.select("a[href~=(?i)\\.(png|jpe?g)]");
+
+                bufferedWriter.append(makeImgComparison(element.attr("href") + files.get(1).attr("href"),element.attr("href") +files.get(1).attr("href")));
+
+            }*/
+
+            //Single Image
             String folderName;
             for(Element element :  doc.select("a[href~=(?i)\\.(dir)]") )
             {
 
                 folderName = "[[Block:" +element.attr("href") + "]] \r\n" ;
-
-    bufferedWriter.append(folderName);
+                bufferedWriter.append(folderName);
 
 
                 doc = Jsoup.connect(link+"/" + element.attr("href")).get();
                 files = doc.select("a[href~=(?i)\\.(png|jpe?g)]");
+
 
                 for (int i=0;i<files.size();i+=2) {
 
@@ -82,11 +100,13 @@ public class GenerateQuestion {
                 }
             }
 
-
-
-
-
             bufferedWriter.close();
+
+
+
+
+
+
         }
         catch(FileNotFoundException ex) {
             System.out.println(
